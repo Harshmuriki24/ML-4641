5247ec3fab4346400c21fe51a9bd266355de55d7
Meikle, Braeden Andrew <bmeikle3@gatech.edu>
2023-03-27 20:57:50 -0400

    Add main function in Main.py and add seaborn code for plotting

--- Main.py
+++ Main.py
@@ -1,8 +1,10 @@
 import Preprocess
 import Model
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
 
-
-def Main():
+def main():
     url = 'higher-education-predictors-of-student-retention/dataset.csv'
     preprocessed_data = Preprocess(url)
     preprocessed_data.clean_data()
@@ -13,3 +15,15 @@ def Main():
     #######THIS IS THE LOGISTIC REGRESSION MODEL############
     regression_model = model.logreg
     #######USE THIS FILE TO CREATE AND PLOT IMAGES##########
+
+    sns.set_theme()
+    dataset = pd.read_csv(url) # read the unfiltered data
+
+    sns.relplot(
+        data=dataset
+    )
+
+    plt.show() # show the plot
+
+if __name__ == "__main__":
+    main()
